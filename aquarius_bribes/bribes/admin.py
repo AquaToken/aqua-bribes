40bdfda45f652d969737c65a8ccd4a3e7d896809
hidden-pond <53864584+hidden-pond@users.noreply.github.com>
2022-02-24 18:46:37 +0300

    fixed admin

--- aquarius_bribes/bribes/admin.py
+++ aquarius_bribes/bribes/admin.py
@@ -10,26 +10,26 @@ class BribeAdmin(admin.ModelAdmin):
     ]
     list_filter = ['status',]
     readonly_fields = [
-        'status', 'message', 'market_key', 'sponsor', 'amount', 'asset_code',
+        'status', 'message', 'market_key_id', 'sponsor', 'amount', 'asset_code',
         'asset_issuer', 'amount_for_bribes', 'amount_aqua', 'convertation_tx_hash',
         'claimable_balance_id', 'paging_token', 'unlock_time', 'start_at',
         'stop_at', 'created_at', 'loaded_at', 'updated_at',
     ]
-    search_fields = ['market_key', 'claimable_balance_id', 'sponsor',]
+    search_fields = ['market_key_id', 'claimable_balance_id', 'sponsor',]
     ordering = ['-loaded_at',]
 
     def get_short_market_key(self, obj):
-        return '{}...{}'.format(obj.market_key[:4], obj.market_key[-4:])
+        return '{}...{}'.format(obj.market_key_id[:4], obj.market_key_id[-4:])
 
 
 @admin.register(AggregatedByAssetBribe)
 class AggregatedByAssetBribeAdmin(admin.ModelAdmin):
     list_display = [
-        '__str__', 'asset_code', 'asset_issuer', 'market_key',
+        '__str__', 'asset_code', 'asset_issuer', 'market_key_id',
         'daily_amount', 'total_reward_amount', 'start_at', 'stop_at', 'created_at',
     ]
     readonly_fields = [
-        'asset_code', 'asset_issuer', 'market_key', 'start_at', 'stop_at', 'created_at', 'updated_at',
+        'asset_code', 'asset_issuer', 'market_key_id', 'start_at', 'stop_at', 'created_at', 'updated_at',
     ]
-    search_fields = ['market_key',]
+    search_fields = ['market_key_id',]
     ordering = ['-created_at',]
