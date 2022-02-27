c136dac777a97bbd1bf65022dd7f57474fe6d267
hidden-pond <53864584+hidden-pond@users.noreply.github.com>
2022-02-27 11:42:57 +0300

    Enable return task

--- aquarius_bribes/taskapp/__init__.py
+++ aquarius_bribes/taskapp/__init__.py
@@ -26,11 +26,11 @@ def setup_periodic_tasks(sender, **kwargs):
             'schedule': crontab(hour='*', minute='0'),
             'args': (),
         },
-        # 'aquarius_bribes.bribes.tasks.task_return_bribes': {
-        #     'task': 'aquarius_bribes.bribes.tasks.task_return_bribes',
-        #     'schedule': crontab(hour='9', minute='0', day_of_week='sunday'),
-        #     'args': (),
-        # },
+        'aquarius_bribes.bribes.tasks.task_return_bribes': {
+            'task': 'aquarius_bribes.bribes.tasks.task_return_bribes',
+            'schedule': crontab(hour='9', minute='0', day_of_week='sunday'),
+            'args': (),
+        },
         'aquarius_bribes.bribes.tasks.task_claim_bribes': {
             'task': 'aquarius_bribes.bribes.tasks.task_claim_bribes',
             'schedule': crontab(hour='0', minute='0', day_of_week='sunday'),
@@ -38,7 +38,7 @@ def setup_periodic_tasks(sender, **kwargs):
         },
         'aquarius_bribes.bribes.tasks.task_aggregate_bribes': {
             'task': 'aquarius_bribes.bribes.tasks.task_aggregate_bribes',
-            'schedule': crontab(hour='12', minute='0', day_of_week='sunday'),
+            'schedule': crontab(hour='9', minute='0', day_of_week='sunday'),
             'args': (),
         },
         # 'aquarius_bribes.bribes.tasks.task_run_load_votes': {
