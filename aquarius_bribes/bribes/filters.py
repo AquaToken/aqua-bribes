f02451720c613d8b49dd697e26e2c36d930a5e2b
hidden-pond <53864584+hidden-pond@users.noreply.github.com>
2022-03-03 16:59:24 +0300

    Extended filter

--- aquarius_bribes/bribes/filters.py
+++ aquarius_bribes/bribes/filters.py
@@ -7,9 +7,9 @@ class BribeFilter(FilterSet):
     class Meta:
         model = Bribe
         fields = {
-            'start_at': ['lt', 'gt'],
-            'stop_at': ['lt', 'gt'],
+            'start_at': ['lt', 'gt', 'gte', 'lte'],
+            'stop_at': ['lt', 'gt', 'gte', 'lte'],
             'asset_code': ['exact', ],
             'asset_issuer': ['exact', 'isnull'],
-            'aqua_total_reward_amount_equivalent': ['lt', 'gt'],
+            'aqua_total_reward_amount_equivalent': ['lt', 'gt', 'gte', 'lte'],
         }
