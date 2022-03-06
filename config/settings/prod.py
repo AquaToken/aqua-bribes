f41cba98af4caccfd21a08c5a58a6c21f8a5195e
hidden-pond <53864584+hidden-pond@users.noreply.github.com>
2022-03-06 17:38:34 +0300

    Use redis

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -25,6 +25,20 @@ DATABASES = {
     'default': env.db(),
 }
 
+# Cache
+# --------------------------------------------------------------------------
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient"
+        },
+        "KEY_PREFIX": "aquarius",
+    },
+}
+
 
 # Template
 # --------------------------------------------------------------------------
