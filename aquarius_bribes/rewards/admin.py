1851d99f5816226fcd0ca372fee12a744d5034bf
hidden-pond <53864584+hidden-pond@users.noreply.github.com>
2023-01-10 12:55:45 +0300

    Improved admin

--- aquarius_bribes/rewards/admin.py
+++ aquarius_bribes/rewards/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
 
+from aquarius_bribes.bribes.models import AggregatedByAssetBribe
 from aquarius_bribes.rewards.models import AssetHolderBalanceSnapshot, Payout, VoteSnapshot
+
+
+class AssetListFilter(admin.SimpleListFilter):
+    title = 'asset'
+    parameter_name = 'asset_code'
+    related_filter_parameter = 'asset_code'
+
+    def lookups(self, request, model_admin):
+        return AggregatedByAssetBribe.objects.all().values_list('asset_code', 'asset_code').distinct()
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(asset_code=self.value())
+        return queryset
 
 
 @admin.register(VoteSnapshot)
@@ -24,10 +39,11 @@ class PayoutAdmin(admin.ModelAdmin):
         return '{}...{}'.format(obj.bribe.market_key_id[:8], obj.bribe.market_key_id[-8:])
     get_short_market_key.short_description = 'Market key'
 
+
 @admin.register(AssetHolderBalanceSnapshot)
 class AssetHolderBalanceSnapshotAdmin(admin.ModelAdmin):
     list_display = ('asset_code', 'get_asset_issuer', 'account', 'balance', 'created_at')
-    list_filter = ('asset_code', 'created_at')
+    list_filter = (AssetListFilter, 'created_at')
     search_fields = ('account', 'asset_code', 'asset_issuer')
 
     def get_asset_issuer(self, obj):
