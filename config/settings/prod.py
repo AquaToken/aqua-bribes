ec8f16c0f946646116666c2e4dca00ae47d577ec
hidden-pond <53864584+hidden-pond@users.noreply.github.com>
2023-08-01 16:30:21 +0300

    Updated horizon

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -178,7 +178,8 @@ if SENTRY_ENABLED:
 # --------------------------------------------------------------------------
 
 STELLAR_PASSPHRASE = 'Public Global Stellar Network ; September 2015'
-HORIZON_URL = 'https://horizon.stellar.lobstr.co'
+# HORIZON_URL = 'https://horizon.stellar.lobstr.co'
+HORIZON_URL = 'https://aqua.network/horizon'
 
 # Bribe configuration
 # --------------------------------------------------------------------------
