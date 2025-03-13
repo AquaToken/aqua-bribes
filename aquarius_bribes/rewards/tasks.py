30165b9a2c61f54665daa400d525c8bc126c74f9
hidden-pond <53864584+hidden-pond@users.noreply.github.com>
2025-03-13 14:33:18 +0300

    Turn on payouts

--- aquarius_bribes/rewards/tasks.py
+++ aquarius_bribes/rewards/tasks.py
@@ -106,8 +106,6 @@ def task_make_trustees_snapshot(snapshot_time=None):
     time_limit=PAYREWARD_TIME_LIMIT.total_seconds() + 60 * 3,
 )
 def task_pay_rewards(snapshot_time=None, reward_period=DEFAULT_REWARD_PERIOD):
-    return
-
     if cache.get(LOAD_VOTES_TASK_ACTIVE_KEY, False) or cache.get(LOAD_TRUSTORS_TASK_ACTIVE_KEY, False):
         return
 
